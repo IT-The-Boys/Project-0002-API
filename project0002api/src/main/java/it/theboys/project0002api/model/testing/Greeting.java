cc1f6273a521f5d6e5287ab966cd3e41f0abcb32
Andreas Lo <2190445@ecc.ac.jp>
2021-10-13 15:41:54 +0900

    Added tests for RESTful and WebSocket

---
+++ project0002api/src/main/java/it/theboys/project0002api/model/testing/Greeting.java
@@ -0,0 +1,16 @@
+package it.theboys.project0002api.model.testing;
+
+public class Greeting {
+    private String content;
+
+    public Greeting(){
+    }
+
+    public Greeting(String content){
+        this.content = content;
+    }
+
+    public String getContent(){
+        return content;
+    }
+}
